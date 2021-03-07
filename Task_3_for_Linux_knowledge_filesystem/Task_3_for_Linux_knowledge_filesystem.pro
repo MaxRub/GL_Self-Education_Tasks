b48c65efe8d6aa99dada0499ef763b72e9a9b98c
maxim <MaxRub>
2021-03-07 15:28:36 +0200

    Task 3 with using <filesystem> C++17

---
+++ Task_3_for_Linux_knowledge_filesystem/Task_3_for_Linux_knowledge_filesystem.pro
@@ -0,0 +1,8 @@
+TEMPLATE = app
+CONFIG += console c++11
+CONFIG += thread
+CONFIG -= app_bundle
+CONFIG -= qt
+
+SOURCES += \
+        main.cpp
---
+++ Task_3_for_Linux_knowledge_filesystem/main.cpp
@@ -0,0 +1,67 @@
+//Create program which count and print file names in some folder
+//using <filesystem>
+#include <iostream>
+#include <cstdlib>                  //for exit()
+#include <cstdio>
+#include <filesystem>               //for work with folder and files
+#include <pthread.h>
+
+void * calculate(void * str);
+void * printPath(void * str);
+
+int main(int argc, char * argv[])
+{
+    using namespace std;
+    using namespace std::filesystem;
+
+    pthread_t first, second;
+    int i1, i2;
+
+    if(argc == 1)                   //check command line argument
+    {
+        cerr << "Usage: " << argv[0] << " no folder specified!\n";
+        exit(EXIT_FAILURE);
+    }
+    if(!exists(argv[1]))            //check directory
+    {
+        cerr << argv[1] << " no existing directory\n";
+        exit(EXIT_FAILURE);
+    }
+    auto path = argv[1];
+
+    //start first tread with calculate()
+    i1 = pthread_create(&first, nullptr, calculate, (void *)path);
+    //start second tread whith printPath()
+    i2 = pthread_create(&second, nullptr, printPath, (void *)path);
+
+    pthread_join(first, nullptr);
+    pthread_join(second, nullptr);
+
+    cout << " Hi!\n";
+    return 0;
+}
+void * calculate(void * str)
+{
+    using namespace std::filesystem;
+    auto temp = (char *)str;
+    int count = 0;
+    //using std::filesystem::directory_iterator for access
+    for(const auto & entry : directory_iterator(temp))
+    {
+        if(exists(entry.path()))        //check file
+            count++;
+    }
+    std::cout << count << " files in folder " << temp << std::endl;
+    return 0;
+}
+void * printPath(void * str)
+{
+    using namespace std::filesystem;
+    auto temp = (char *)str;
+    for(const auto & entry : directory_iterator(temp))
+    {
+        if(exists(entry.path()))
+            std::cout << entry.path() << std::endl;
+    }
+    return 0;
+}
